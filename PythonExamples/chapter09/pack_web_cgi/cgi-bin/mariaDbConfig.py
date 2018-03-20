52c37fdd15aab2564d94ceff328bc1b314fe9bc0
acorn <acorn@acorn-PC>
2018-03-20 09:24:02 +0900

    web 연습

---
+++ PythonExamples/chapter09/pack_web_cgi/cgi-bin/mariaDbConfig.py
@@ -0,0 +1,17 @@
+import MySQLdb
+
+config = {
+    'host':'127.0.0.1', 
+    'user':'root',
+    'password':'wlgml',
+    'database':'nice',
+    'port':3306,
+    'charset':'utf8',
+    'use_unicode':True
+    }
+
+
+# conn = MySQLdb.connect(**config)
+#  
+# print(conn)
+# conn.close()
