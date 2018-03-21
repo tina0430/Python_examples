f28a842f0050fe52546506b6b6c03d29bf10f4b9
tina0430 <leejihee950430@gmail.com>
2018-03-21 12:39:35 +0900

    facebook post 긁어오기

--- dataCrawling/ch05_facebook/getFBNumericID.py
+++ dataCrawling/ch05_facebook/getFBNumericID.py
@@ -1,42 +1,37 @@
 import urllib.request
 import json
+import datetime #카페 177
 
 def getFacebookNumericID(page_name):
-    #[code 1] 기본적인 설정 값 변수 할당
-    with open('id.json', 'r') as myID:
-        jID = json.load(myID)
+    #기본적인 설정 값 변수 할당
+    with open('id.json', 'r') as myID : jID = json.load(myID)
     
     app_id = jID['App_id']
     app_secret = jID['App_secret_code']
-    
     access_token = app_id +'|' + app_secret
     
-    #[code 2] 접근 url 생성
+    #접근 url 생성
     url = "https://graph.facebook.com/v2.8/"    #base
     url += page_name                            #node
     url += "/?access_token=%s"%(access_token)   #token
     print("url : ", url)
     
-    #[code 3] 
+    data = getRequestUrl(url)
+    with open(page_name+'FacebookNID.json', 'w') as idFile : idFile.write(data)
+        
+def getRequestUrl(url):
     req = urllib.request.Request(url)
-    
-    #[code 4]
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            data = response.read()
-            data = data.decode('utf-8')
-            with open(page_name+'FacebookNID.json', 'w') as idFile:
-                idFile.write(data)
-            data = json.loads(data)
-            page_id = data['id']
-            print(page_name + " Facebook Numeric id : " + page_id)
-            
-    except Exception as err:
-        print("error :", err)
-    finally:
-        print('완료')
+            print('==[%s] url request success=='%(datetime.datetime.now()))
+            return response.read().decode('utf-8')  #함수(메소드) 체이닝
         
+    except Exception as err :
+        print('error :', err)  
+        print('==[%s] Error for URL : %s=='%(datetime.datetime.now(), url))  
+        return None  
+      
 if __name__ == "__main__":
     #page_name = 'jtbcnews'
     #page_name = "chosun"
