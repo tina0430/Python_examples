799bbc9818efd6c79190f33a85efcf3dd4bc37db
tina0430 <leejihee950430@gmail.com>
2018-03-22 16:46:00 +0900

    beautifulSoup을 이용하여 네이버 무비에서 영화 랭킹 가져오기

---
+++ dataCrawling/beautifulSoupTest/ex01.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+html = """
+<td class='title'>
+    <div class='tit3' id='hong'>
+        <a href='www.abcd.com' title='abcd'>미녀와 야수</a>
+    </div>
+</td>
+"""
+soup = BeautifulSoup(html, 'html.parser')
+print(type(soup), soup)
+
+print('='*50)
+tag = soup.td
+print(type(tag), tag)
+print(tag['class'])
+print('='*50)
+
+tag.find('div', attrs={'class':'tit3'})
+print(tag)
+tag = soup.div
+print(tag)
+ 
+print('='*50)
+tag = soup.a
+print(tag)
---
+++ dataCrawling/beautifulSoupTest/ex02.py
@@ -0,0 +1,17 @@
+#네이버 무비에서 영화 랭킹 가져오기
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "http://movie.naver.com/movie/sdb/rank/rmovie.nhn"
+
+html = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(html, 'html.parser')
+
+url_header = 'http://movie.naver.com'
+tags = soup.findAll('div', attrs={'class':'tit3'})
+for tag in tags:
+    print(tag.a.text, end = "  -  ")
+    print(url_header+tag.a.get('href'))    #tag.g.['href']
+    print('-'*80)
