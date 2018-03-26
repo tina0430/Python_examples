2204ec8140a3801cf630e98848c63f725e06a919
tina0430 <leejihee950430@gmail.com>
2018-03-26 11:52:02 +0900

    속도향상을 위해 콘솔에 프린트하던 메세지 삭제

--- dataCrawling/bbq/getKyoStore.py
+++ dataCrawling/bbq/getKyoStore.py
@@ -8,11 +8,10 @@ from itertools import count
 
 def getRequestUrl(url):
     req = urllib.request.Request(url)
-    code = 0
     try:
         responce = urllib.request.urlopen(req)
         if responce.getcode() == 200:
-            print('[%s] Url Reqest Success' % datetime.datetime.now())
+#             print('[%s] Url Reqest Success' % datetime.datetime.now())
             return responce.read().decode('utf-8', errors='ignore')
 
     except Exception as err:
@@ -24,9 +23,9 @@ def getKynochonAddress():
 
     for sido_idx in range(1,18):
         for gungu_idx in count():
+            print(sido_idx, gungu_idx)
             url = "http://www.kyochon.com/shop/domestic.asp"
             url += "?sido1=%s&sido2=%s"%(sido_idx, gungu_idx+1)
-            print(url)
             data = getRequestUrl(url)
             
             if data == None:
@@ -37,8 +36,10 @@ def getKynochonAddress():
             liTags = ulTag.findAll('li')
             for li in liTags:
                 address = li.a.get('href')
+                
                 if not address:
                     break
+                
                 address_ = address.split("'")
                 store = address_[3]
                 address = address_[1]
@@ -60,11 +61,11 @@ def main():
     myColumns = ('store', 'sido', 'gundu', 'address')
     myEncoding = 'utf-8'
     
-    print('Kyochon 매장 크롤링 시작')
+    print('[%s] 교촌 매장 크롤링 시작' % datetime.datetime.now())
     result = getKynochonAddress()
     data = pd.DataFrame(result, columns=myColumns)
     data.to_csv('kyochon.csv', encoding=myEncoding, mode ='w', index=True, index_label='idx')
-    print('Kyochon 매장 크롤링 종료')
+    print('[%s] 교촌 매장 크롤링 종료' % datetime.datetime.now())
     
 if __name__ == "__main__":
     main()
