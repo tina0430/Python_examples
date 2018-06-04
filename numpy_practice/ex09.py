77f0e9c399dba93d2aa17454d68105449d79e706
tina0430 <leejihee950430@gmail.com>
2018-06-04 18:30:46 +0900

    비즈 도안 만들기

--- numpy_practice/ex09.py
+++ numpy_practice/ex09.py
@@ -2,31 +2,53 @@ import numpy as np
 
 arr1 = np.array(range(10))
 print(arr1)
-print()
+print("10까지 걍 배열 만들기", "#"*35)
 
 print(arr1[5])
-print()
+print("이제부터 슬라이싱", "#"*38)
 
 print(arr1[5:8])
-print()
+print("#"*50)
 
 arr1[5:8] = 12
 print(arr1[5:8])
-print()
+print("#"*50)
 
 arr_slice = arr1[5:8]
 arr_slice[1] = 100
 print(arr1)
-print()
+print("슬라이싱 끝", "#"*43)
 
 arr2 = np.array(range(9))
 arr2 = np.reshape(arr2, [3, 3])
 
 print(arr2[2])
-print()
+print("#"*50)
 
 print(arr2[0][2])
-print()
+print("#"*50)
 
 print(arr2[0, 2])
-print()
+print("3차원 시작", "#"*43)
+
+arr3d = np.array([[[1,2,3,100], [4,5,6,100]], [[7,8,9,100], [10,11,12,100]], 
+                  [[13,14,15,100], [16,17,18,100]]])
+print(arr3d)
+print(arr3d.shape)
+print("#"*50)
+
+print(arr3d[0])
+print("#"*50)
+
+print(arr3d[:2, :1, :3])
+print("100으로 바꾸기", "#"*39)
+
+old_values = arr3d[0].copy()
+
+arr3d[0] = 100
+print(arr3d)
+print("#"*50)
+
+arr3d[0] = old_values
+print(arr3d)
+print("#"*50)
---
+++ openCV_practice/image_test.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+import json
+import math
+
+#http://opencv-python.readthedocs.io/en/latest/doc/01.imageStart/imageStart.html#id2
+#TODO - 샤프하게
+def get_clost_color(R, G, B, colors):
+    dist_min = 2
+    for col in colors.values():
+        R_ = col["R"]
+        G_ = col["G"]
+        B_ = col["B"]
+        dist = math((R-R_)**2+(G-G_)**2+(B-B_)**2)
+        if dist < dist_min:
+            dist_min = dist
+            close_R = R_
+            close_G = G_
+            close_B = B_
+    
+def draw_dots(img_file, colors):
+    
+    img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+    rows, cols = img.shape[:2]
+    unit  = 10
+    print(img.shape)
+    
+    new_rows = rows-(rows%unit)+unit if rows%unit != 0 else rows
+    new_cols = cols-(cols%unit)+unit if cols%unit != 0 else cols
+    dst = np.zeros((new_rows, new_cols, 3))
+    print(dst.shape)
+    
+    R = G = B = 0
+    for i in range(0, new_rows, unit):
+        for j in range(0, new_cols, unit):
+            dot = img[i:i+unit, j:j+unit]
+    #         print(i, i+unit, j, j+unit)
+    #         print(i,j, "="*50)
+            if i < rows:
+                B= dot[:unit, :unit, :1].mean()/255
+                G= dot[:unit, :unit, 1:2].mean()/255
+                R= dot[:unit, :unit, 2:3].mean()/255
+                
+                
+            dst[i:i+unit, j:j+unit, :1] = B
+            dst[i:i+unit, j:j+unit, 1:2] = G
+            dst[i:i+unit, j:j+unit, 2:3] = R
+            
+    #         print(dot[:unit, :unit, :1].mean(), dot[:unit, :unit, 1:2].mean(), dot[:unit, :unit, 2:3].mean())
+    #         print(dst[i:i+unit, j:j+unit, 0:1], dst[i:i+unit, j:j+unit, 1:2], dst[i:i+unit, j:j+unit, 2:3])
+    
+    cv2.imshow('image', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    color_file = r'./colors.json'
+    colors = {}
+    
+    with open(color_file, 'r') as f:
+        colors = json.load(f)
+
+
+    img_file = r'c:\marill.png'
+    draw_dots(img_file, colors)
