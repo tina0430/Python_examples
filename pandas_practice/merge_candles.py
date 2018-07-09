563b6c27958a5a5cf81636d780ca0361d2c422c5
tina0430 <leejihee950430@gmail.com>
2018-07-10 03:09:45 +0900

    불필요한 print 삭제

--- pandas_practice/merge_candles.py
+++ pandas_practice/merge_candles.py
@@ -3,19 +3,15 @@ import pandas as pd
 file_30s = './candle/candle_30s.csv'
 table = pd.read_csv(file_30s, header=0, encoding='CP949')
 len_30s = len(table) 
-print(len_30s)
 
 file_1m = './candle/candle_1m.csv'
 table_1m = pd.read_csv(file_1m, header=0, encoding='CP949')
-print(len(table_1m))
     
 file_3m = './candle/candle_3m.csv'
 table_3m = pd.read_csv(file_3m, header=0, encoding='CP949')
-print(len(table_3m))
 
 file_30m = './candle/candle_30m.csv'
 table_30m = pd.read_csv(file_30m, header=0, encoding='CP949')
-print(len(table_30m))
 
 print('='*30)
 for i in range(len_30s) :
