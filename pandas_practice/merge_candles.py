4dda4bb187507eeed368f49b23657ecd0fe5e6ff
tina0430 <leejihee950430@gmail.com>
2018-07-10 20:22:17 +0900

    실제 파일에 맞게 수정

--- pandas_practice/merge_candles.py
+++ pandas_practice/merge_candles.py
@@ -1,23 +1,65 @@
 import pandas as pd
 
-file_30s = './candle/candle_30s.csv'
-table = pd.read_csv(file_30s, header=0, encoding='CP949')
-len_30s = len(table) 
+files = ['./candle/30초.csv', './candle/1분.csv', './candle/3분.csv', './candle/30분.csv']
 
-file_1m = './candle/candle_1m.csv'
-table_1m = pd.read_csv(file_1m, header=0, encoding='CP949')
+# table_30s = pd.read_csv(files[0], header=0, encoding='CP949')
+# len_30s = len(table_30s) 
+
+table_1m = pd.read_csv(files[1], header=0, encoding='CP949')
+
+table_3m = pd.read_csv(files[2], header=0, encoding='CP949')
+table_3m = table_3m.filter(['시간', 'OBV+Slow%K'], axis=1)
+
+table_30m = pd.read_csv(files[3], header=0, encoding='CP949')
+table_30m = table_30m.filter(['시간', 'OBV+Slow%K'], axis=1)
+
+len_1m = len(table_1m) 
+len_3m = len(table_3m) 
+len_30m = len(table_30m) 
+
+print(len_1m)
+print(len_3m)
+
+table = table_1m.filter(['일자','시간', 'OBV+Slow%K'], axis=1)
+table = table.rename(columns={'시간':' 시간_1분', 'OBV+Slow%K':'OBV+Slow%K_1분'})
+print(table.head(5))
+i_3m = i_30m = 0
+
+
+for i in range(len_1m) :
+    tmp = table_3m["시간"][i_3m]
+    table.set_value(i, '시간_3분', tmp)
     
-file_3m = './candle/candle_3m.csv'
-table_3m = pd.read_csv(file_3m, header=0, encoding='CP949')
+    tmp = table_3m["OBV+Slow%K"][i_3m]
+    table.set_value(i, 'OBV+Slow%K_3분', tmp)
+     
+    tmp = table_30m["시간"][i_30m]
+    table.set_value(i, '시간_30분', tmp)
 
-file_30m = './candle/candle_30m.csv'
-table_30m = pd.read_csv(file_30m, header=0, encoding='CP949')
+    tmp = table_30m["OBV+Slow%K"][i_30m]
+    table.set_value(i, 'OBV+Slow%K_30분', tmp)
+      
+    if i%3 == 0 and i_3m < len_3m-1:
+        i_3m = i_3m+1
+         
+    if i%30 == 0 and i_30m < len_30m-1:
+        i_30m = i_30m+1
+        
+#     30초 기준
+#     tmp = table_1m["OBV+Slow%K_1분"][int(i/2)]
+#     table.set_value(i, 'OBV+Slow%K_1분', tmp)
+#     tmp = table_1m["시간"][int(i/2)]
+#     table.set_value(i, '시간_1분', tmp)
+#     tmp = table_3m["OBV+Slow%K"][int(i/6)]
+#     table.set_value(i, 'OBV+Slow%K_3분', tmp)
+#     tmp = table_3m["시간"][int(i/6)]
+#     table.set_value(i, '시간_3분', tmp)
+#     
+#     tmp = table_30m["OBV+Slow%K"][int(i/60)]
+#     table.set_value(i, 'OBV+Slow%K_30분', tmp)
+#     tmp = table_30m["시간"][int(i/60)]
+#     table.set_value(i, '시간_30분', tmp)
 
-print('='*30)
-for i in range(len_30s) :
-    tmp = table_1m["time"][int(i/2)]
-    table.set_value(i, 'val_1m', tmp)
-    tmp = table_3m["time"][int(i/6)]
-    table.set_value(i, 'val_3m', tmp)
-    tmp = table_30m["time"][int(i/60)]
-    table.set_value(i, 'val_30m', tmp)
+print(table.fillna('0.00%'))
+filename = './candle/merge_candles.csv'
+table.to_csv(filename, index = False, encoding='cp949')
